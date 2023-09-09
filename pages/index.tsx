ffd168fbe32ba17ae7536069156cf471cba9ad0c
Vivek <63992417+vdutts7@users.noreply.github.com>
2023-09-09 03:42:23 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>Scorphx | Footy Wallpapers</title>
         <meta
           property="og:image"
           content="https://nextjsconf-pics.vercel.app/og-image.png"
